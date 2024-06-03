10dea7ef494d5a91d68905f0715399972ae24b90
PeterHW963 <135524242+PeterHW963@users.noreply.github.com>
2024-06-03 10:23:50 +0800

    editor basics finished
    i havent figured out how to make filter = nearest. And i need to handle crawler ground to only be placeable on ground. And i need to handle the crawler air to have variable string length

--- Scenes/Editor_screen.gd
+++ Scenes/Editor_screen.gd
@@ -7,7 +7,7 @@ const CRAWLER2_SCENE = preload("res://crawler_box_2.tscn")
 
 
 func _can_drop_data(position, data):
-	return data.has("instance")
+	return data.has("instance") and data.has("type")
 
 func _drop_data(position, data):
 	if data.has("instance"):
@@ -17,25 +17,51 @@ func _drop_data(position, data):
 		instance.position = position 
 		instance.scale = Vector2(3,3)
 		
-		convert_to_area2d(instance)
+		match data["type"]:
+			"ghaster":
+				handle_ghaster(instance)
+			"flapper":
+				handle_flapper(instance)
+			"crawler1":
+				handle_crawler1(instance)
+			"crawler2":
+				handle_crawler2(instance)
+
+func handle_ghaster(instance):
+	print("Handling Ghaster")
+	convert_to_area2d(instance)
+
+func handle_flapper(instance):
+	print("Handling Flapper")
+	convert_to_area2d(instance, true)
+
+func handle_crawler1(instance):
+	print("Handling Crawler 1")
+	convert_to_area2d(instance)
+
+func handle_crawler2(instance):
+	print("Handling Crawler 2")
+	convert_to_area2d(instance)
 		
-func convert_to_area2d(instance):
+func convert_to_area2d(instance, flip_horizontal = false):
 	print("converting")
 	var texture_button = instance.get_node("TextureButton")
-	var ghaster_area2d = Area2D.new()
+	var entity_area2d = Area2D.new()
 	
 	var sprite = Sprite2D.new()
 	sprite.texture = texture_button.texture_normal
-	sprite.scale = texture_button.scale
-	#if sprite.texture:
-	#	var texture = sprite.texture as Texture2D
-	#	texture.filter_mode = TEXTURE_FILTER_NEAREST
-	# i havent figured out how to make it have the nearest filter
 	
-	ghaster_area2d.add_child(sprite)
-	ghaster_area2d.position = instance.position
-	ghaster_area2d.scale = Vector2(3,3)
-	add_child(ghaster_area2d)
+	# HOW TF TO MAKE SPRITE.TEXTURE FILTER = NEAREST
+	
+	if flip_horizontal:
+		sprite.scale = Vector2(-texture_button.scale.x, texture_button.scale.y)
+	else:
+		sprite.scale = texture_button.scale
+	
+	entity_area2d.add_child(sprite)
+	entity_area2d.position = instance.position
+	entity_area2d.scale = Vector2(3,3)
+	add_child(entity_area2d)
 	print("Converted")
 	instance.queue_free()
 
