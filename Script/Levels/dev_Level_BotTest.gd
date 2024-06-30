4d00599aff81e50edf25dc5b8cd93c35cf0e9092
PeterHW963 <135524242+PeterHW963@users.noreply.github.com>
2024-07-01 04:59:15 +0800

    level 2

--- Script/Levels/dev_Level_BotTest.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-
-var last_enemy_x = 2000
-
-var enemy_data = {
-	0: {"position": Vector2(1100, 420), "type": "gh"},
-	1: {"position": Vector2(2000, 560), "type": "gh"},
-	2: {"position": Vector2(2000, 420), "type": "gh"},
-}
--- Script/Levels/sexnow.gd
+++
@@ -1,9 +0,0 @@
-var is_completed = false
-var is_saved = false
-
-var last_enemy_x = 966
-
-var enemy_data = {
-	0: {"position": Vector2(959, 160), "type": "gh"},
-	1: {"position": Vector2(966, 529), "type": "fl"},
-}
