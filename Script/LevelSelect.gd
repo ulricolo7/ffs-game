8b7342eb1cb0c4c06cca67adc65302a5fa38e5b5
PeterHW963 <135524242+PeterHW963@users.noreply.github.com>
2024-07-05 17:39:07 +0800

    instructions button and make new level button

--- Script/LevelSelect.gd
+++ Script/LevelSelect.gd
@@ -2,6 +2,7 @@ extends Panel
 
 @export var levels_folder: String = "res://Script/Levels/"
 @export var button_scene = preload("res://Scenes/level_button.tscn")
+const make_new_lvl_button_scene = preload("res://Scenes/make_new_lvl_button.tscn")
 
 signal level_selected
 
@@ -11,6 +12,10 @@ func _ready():
 	else:
 		$open_file_quit.visible = false
 	_scan_levels_folder()
+	var make_new_lvl_instance = make_new_lvl_button_scene.instantiate()
+	make_new_lvl_instance.connect("pressed", Callable(self, "open_editor"))
+	$ScrollContainer/VBoxContainer.add_child(make_new_lvl_instance)
+	
 
 func _scan_levels_folder():
 	var dir = DirAccess.open(levels_folder)
@@ -61,10 +66,8 @@ func is_level_saved(file_path: String) -> bool:
 func _add_level_button(file_path: String):
 	var button_instance = button_scene.instantiate()
 	var name_label = button_instance.get_node("level_button/VBoxContainer/LevelName")
-	#var desc_label = button_instance.get_node("Button/level_button/VBoxContainer/LevelDesc")
 	var last_edited_label = button_instance.get_node("level_button/VBoxContainer/LastEdited")
 	name_label.text = file_path.get_file().get_basename()
-	#desc_label.text = file_path.get_file()
 	last_edited_label.text = "Last edited: " + _get_file_last_modified(file_path)
 	var lvl_name = name_label.text
 	
@@ -135,3 +138,8 @@ func _on_delete_lvl_button_pressed(button_instance, file_path: String):
 		Main.CACHED_EDITOR_LEVEL = ""
 	if Main.CACHED_EDITOR_LEVEL_COMPLETED == file_path:
 		Main.CACHED_EDITOR_LEVEL_COMPLETED = ""
+	
+func open_editor():
+	print("reached")
+	Main.in_editor = true
+	get_tree().change_scene_to_file("res://Scenes/editor.tscn")
