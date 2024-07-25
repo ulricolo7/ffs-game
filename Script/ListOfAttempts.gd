1dbe8bec45aa98f9bcd713548b39c9d66a76e585
PeterHW963 <135524242+PeterHW963@users.noreply.github.com>
2024-07-25 09:16:27 +0800

    added records button

--- Script/ListOfAttempts.gd
+++ Script/ListOfAttempts.gd
@@ -2,4 +2,7 @@ var previous_attempts = {
 	0: {"player": "Player", "level": "dev_Level_1", "progress": 64.3, "time": "2024-07-25T05:35:40"},
 	1: {"player": "MAIDEN[Easy]", "level": "dev_Level_1", "progress": 49.1, "time": "2024-07-25T05:40:19"},
 	2: {"player": "Player", "level": "forricoluv", "progress": 100, "time": "2024-07-25T08:18:08"},
+	3: {"player": "Player", "level": "dev_Level_5", "progress": 12, "time": "2024-07-25T09:14:47"},
+	4: {"player": "Player", "level": "dev_Level_5", "progress": 6.9, "time": "2024-07-25T09:14:50"},
+	5: {"player": "DRAGON[Hard]", "level": "dev_Level_5", "progress": 100, "time": "2024-07-25T09:15:23"},
 }
